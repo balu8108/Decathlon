3b40e96d8487ad3a1823647516cafe8ffe4ba2a6
balu8108 <atluribalachandar8108@gmail.com>
2021-09-08 03:30:45 +0530

    initial commit

---
+++ app/src/main/java/com/bala/nytnews/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bala.nytnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bala.nytnews.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
